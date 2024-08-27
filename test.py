19eac2c6dc98684d3f3fe1702e8809e60642f3f8
Eli <86804975+libraryguyeli@users.noreply.github.com>
2024-08-27 14:18:41 +0000

    Testing CSV functionality for VSCode web version

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 import random
+import csv
 
 print(random.randint(1,1000))
-
-for i in range(25):
-    print(i)
 
 def add(a, b):
     return a + b
 
-print(add(1, 2))
+print("ADD:", add(1, 2))
+
+with open("customers-100.csv", "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        if int(row['Index']) % 5 == 0:
+            print(f"{row['First Name']} {row['Last Name']}, {row['Company']}")
