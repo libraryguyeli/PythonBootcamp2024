f1366e703d0cbf06f98b69bd5de85618a77b763d
Eli <86804975+libraryguyeli@users.noreply.github.com>
2024-08-27 14:27:09 +0000

    Testing txt files with VSCode web version

--- test.py
+++ test.py
@@ -8,8 +8,14 @@ def add(a, b):
 
 print("ADD:", add(1, 2))
 
-with open("customers-100.csv", "r") as file:
-    reader = csv.DictReader(file)
+with open("customers-100.csv", "r") as csv_file:
+    reader = csv.DictReader(csv_file)
     for row in reader:
         if int(row['Index']) % 5 == 0:
             print(f"{row['First Name']} {row['Last Name']}, {row['Company']}")
+
+with open("test.txt", "a") as txt_file:
+    for i in range(1,11):
+        txt_file.write("#" * i + "\n")
+    for i in range(11,1,-1):
+        txt_file.write("#" * i + "\n")
